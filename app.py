3d7ff06b845fbb8608efc736e652bff06a6860bb
gabrielalvescosta <gabriel.a.costa23@aluno.senai.br>
2026-03-27 21:33:53 -0300

    a

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from controllers.auth_controller import auth_bp
 from controllers.receita_controller import receita_bp
@@ -6,6 +7,8 @@ app = Flask(__name__)
 
 # SEM ISSO, A SESSÃO NÃO FUNCIONA E O LOGIN É IGNORADO!
 app.secret_key = "sabor_do_brasil_chave_secreta_2024" 
+
+ARQUIVO_DADOS = "usuarios.json"
 
 # Aqui embaixo devem estar os registros dos seus controllers/blueprints
 app.register_blueprint(auth_bp)
