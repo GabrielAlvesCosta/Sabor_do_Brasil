1cd069984e79af9ea7d13e7a42ec371427f626f8
gabrielalvescosta <gabriel.a.costa23@aluno.senai.br>
2026-03-27 23:16:03 -0300

    a

--- utils/utils.py
+++ utils/utils.py
@@ -26,7 +26,6 @@ def hash_senha(senha: str) -> str:
     return hash_bytes.decode("utf-8")
 
 def verificar_senha(senha_texto_puro: str, senha_hash: str) -> bool:
-
     senha_bytes = senha_texto_puro.encode("utf-8")
     hash_bytes = senha_hash.encode("utf-8")
     return checkpw(senha_bytes, hash_bytes)
